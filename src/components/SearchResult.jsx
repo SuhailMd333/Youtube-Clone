c43ac1c6d905c1dbe2270841480a97c3f6e073dc
Suhail <suhailshk333@gmail.com>
2023-06-24 11:37:29 +0530

    done

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+
+import { fetchDataFromApi } from "../utils/Api";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import SearchResultVideoCard from "./SearchResultVideoCard";
 
 const SearchResult = () => {
+
+  const [result,setresult] = useState();
+  const {searchQuery} = useParams();
+  const {setloading} = useContext(Context);
+  useEffect(()=> {
+
+document.getElementById("root").classList.remove("custom-h");
+fetchSearchResult();
+  },[searchQuery])
+
+
+const fetchSearchResult = () => {
+  setloading(true)
+fetchDataFromApi(`search/?q=${searchQuery}`).then((res) => {
+  console.log(res)
+setresult(res?.contents)
+setloading(false)
+
+})
+}
   return (
-    <div>
-      SearchResult
+   
+    <div className="flex flex-row h-[calc(100%-56px)]">
+    <LeftNav />
+    <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+
+{
+  result?.map((item) => {
+    if(item.type !== "video") return false;
+         let video = item.video;
+    return(
+      <SearchResultVideoCard
+      video={video}
+      key={video?.videoId}
+      />
+    )
+  })
+}
+        </div>
+      </div>
     </div>
   )
 }
